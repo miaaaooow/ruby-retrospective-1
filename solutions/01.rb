79df5faa0d68fd751d81dda5590ba90d9d4b7cfa
miaaaooow <mateva.maria@gmail.com>
2011-11-12 13:02:02 +0200

    initial commit

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,21 +1,56 @@
 class Array
-  def to_hash
-    {}.tap do |result|
-      each { |key, value| result[key] = value }
+    
+    def to_hash
+        Hash[*self.flatten]
     end
-  end
 
-  def index_by
-    map { |n| [yield(n), n] }.to_hash
-  end
 
-  def subarray_count(subarray)
-    each_cons(subarray.length).count(subarray)
-  end
-
-  def occurences_count
-    Hash.new(0).tap do |result|
-      each { |item| result[item] += 1 }
+    def index_by
+        res = {}
+        if block_given? 
+            self.each do |x|
+                res[yield(x)] = x
+            end
+        end
+        res
     end
-  end
+
+    
+    def subarray_count(subarray)
+        count = 0
+        len = self.length
+        for i in 0...len
+            if full_match?(subarray, i)
+                count += 1
+            end
+        end
+        count
+    end
+
+
+    def occurences_count
+        res = Hash.new(0) 
+        self.map { |elem| res[elem] += 1 }
+        res
+    end
+
+    #as ugly as I could :/
+    private 
+    def full_match? (arr2, pos)
+        m = self.length
+        n = arr2.length
+        if (n > m - pos)
+            false
+        else 
+            for i in 0...n  
+                if self[i + pos] != arr2[i] 
+                    return false 
+                end
+            end
+            true
+        end
+    end
+           
 end
+
+
